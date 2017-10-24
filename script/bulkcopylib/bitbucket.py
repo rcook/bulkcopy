e5fe791bdfed8dbb033928be4a4932a6359625b6
Richard Cook <rcook@rcook.org>
2017-10-24 05:13:30 -0700

    Rename Bitbucket fields

--- script/bulkcopylib/bitbucket.py
+++ script/bulkcopylib/bitbucket.py
@@ -6,29 +6,29 @@ _BITBUCKET_AUTH_URL = "https://bitbucket.org/site/oauth2/authorize"
 _BITBUCKET_TOKEN_URL = "https://bitbucket.org/site/oauth2/access_token"
 
 class _BitbucketUrlProvider(object):
-    def __init__(self, bitbucket_key, bitbucket_secret):
-        self._bitbucket_key = bitbucket_key
-        self._bitbucket_secret = bitbucket_secret
-        self._bitbucket = None
+    def __init__(self, api_key, api_secret):
+        self._api_key = api_key
+        self._api_secret = api_secret
+        self._client = None
 
     def get(self, url):
         self._do_oauth_dance()
-        response = self._bitbucket.get(url)
+        response = self._client.get(url)
         response.raise_for_status()
         return response.content
 
     def _do_oauth_dance(self):
-        if self._bitbucket is None:
-            self._bitbucket = OAuth2Session(self._bitbucket_key)
-            auth_url = self._bitbucket.authorization_url(_BITBUCKET_AUTH_URL)
+        if self._client is None:
+            self._client = OAuth2Session(self._api_key)
+            auth_url = self._client.authorization_url(_BITBUCKET_AUTH_URL)
             print("Please go here and authorize: {}".format(auth_url[0]))
             redirect_response = raw_input("Paste full redirect URL here: ")
-            self._bitbucket.fetch_token(
+            self._client.fetch_token(
                 _BITBUCKET_TOKEN_URL,
                 authorization_response=redirect_response,
-                username=self._bitbucket_key,
-                password=self._bitbucket_secret)
+                username=self._api_key,
+                password=self._api_secret)
 
-def make_bitbucket_url_cache(bitbucket_key, bitbucket_secret, cache_dir):
-    url_provider = _BitbucketUrlProvider(bitbucket_key, bitbucket_secret)
+def make_bitbucket_url_cache(api_key, api_secret, cache_dir):
+    url_provider = _BitbucketUrlProvider(api_key, api_secret)
     return UrlCache(url_provider, cache_dir)
