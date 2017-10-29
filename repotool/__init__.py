b566fb270b59d0085ecb41bf96eb2a6d5a7600ca
Richard Cook <rcook@rcook.org>
2017-10-28 21:03:26 -0700

    Move requirements into setup.py

--- repotool/__init__.py
+++ repotool/__init__.py
@@ -1,3 +1,7 @@
 ##################################################
 # Copyright (C) 2017, All rights reserved.
 ##################################################
+
+__project_name__ = "repotool"
+__version__ = "0.0"
+__description__ = "Python wrappers around GitHub, GitLab and Bitbucket REST APIs"
