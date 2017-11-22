5ec21b05b504fd9ea1cde8bcd509a6b847c16269
Richard Cook <rcook@rcook.org>
2017-11-22 14:52:08 -0800

    Bump to version 0.4

--- repotool/__init__.py
+++ repotool/__init__.py
@@ -3,5 +3,5 @@
 ##################################################
 
 __project_name__ = "repotool"
-__version__ = "0.3"
+__version__ = "0.4"
 __description__ = "Python wrappers around GitHub, GitLab and Bitbucket REST APIs"
